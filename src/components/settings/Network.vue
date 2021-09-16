7b178967083e1ed5c1cceced7aad438da7fcf0b8
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-09-16 20:30:30 +0700

    Fix network

--- src/components/settings/Network.vue
+++ src/components/settings/Network.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { SUPPORTED_NETWORKS } from "@toruslabs/casper-controllers";
-import { ref, watch } from "vue";
+import { computed } from "vue";
 
 import { SelectField } from "@/components/common";
 import ControllersModule from "@/modules/controllers";
@@ -12,18 +12,18 @@ const networks = Object.keys(SUPPORTED_NETWORKS).map((key) => {
     value: value.chainId,
   };
 });
-const selectedNetwork = ref(networks[0]);
-watch(selectedNetwork, (newValue) => {
-  if (!newValue) selectedNetwork.value = networks[0];
-  ControllersModule.setNetwork(newValue.value);
+const selectedNetwork = computed({
+  get: () => networks.find((it) => it.value === ControllersModule.torusState.NetworkControllerState.chainId),
+  set: (value) => {
+    if (value) ControllersModule.setNetwork(value.value);
+  },
 });
-
-// TODO: Handle when chainId is "loading"
 </script>
 <template>
   <div class="pb-4">
     <div class="mb-4">
-      <SelectField v-model="selectedNetwork" label="Select Network" :items="networks" />
+      <SelectField v-if="selectedNetwork" v-model="selectedNetwork" label="Select Network" :items="networks" />
+      <div v-else class="block text-sm font-body text-app-text-600 dark:text-app-text-dark-500 mb-16">Loading network...</div>
     </div>
   </div>
 </template>
