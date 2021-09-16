04f07201e818665625252f16074f4f46e5e959b9
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-09-16 17:58:40 +0700

    Hook up network

--- src/components/common/NetworkDisplay.vue
+++ src/components/common/NetworkDisplay.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { WifiIcon } from "@heroicons/vue/outline";
+
+import ControllersModule from "@/modules/controllers";
+const network = ControllersModule.torusState.NetworkControllerState.providerConfig.displayName;
 </script>
 
 <template>
-  <div class="rounded-xl flex items-center bg-gray-200 text-xs font-medium py-1 px-4"><WifiIcon class="w-3 h-3 mr-1" /> Casper Network</div>
+  <div class="rounded-xl flex items-center bg-gray-200 text-xs font-medium py-1 px-4"><WifiIcon class="w-3 h-3 mr-1" />{{ network }}</div>
 </template>
 
 <style scoped></style>
