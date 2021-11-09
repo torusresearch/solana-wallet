48f0b88c6149b3db77a0b3b69c53f79adb8f7c75
cwlee <cwlee0@gmail.com>
2021-11-09 12:44:15 +0800

    refactor code

--- src/pages/RedirectHandler.vue
+++ src/pages/RedirectHandler.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { BROADCAST_CHANNELS, broadcastChannelOptions, POPUP_RESULT, RedirectHandler } from "@toruslabs/base-controllers";
+import { BROADCAST_CHANNELS, broadcastChannelOptions, RedirectHandler } from "@toruslabs/base-controllers";
 import { BroadcastChannel } from "broadcast-channel";
 import { onMounted } from "vue";
 
@@ -12,10 +12,11 @@ onMounted(async () => {
 const checkTopupSuccess = async () => {
   const queryParameters = new URLSearchParams(window.location.search);
   const instanceId = queryParameters.get("instanceId");
-  const success = queryParameters.get("topup");
-  if (success) {
+  const topupResult = queryParameters.get("topup");
+  if (topupResult === "success") {
     const bc = new BroadcastChannel(`${BROADCAST_CHANNELS.REDIRECT_CHANNEL}_${instanceId}`, broadcastChannelOptions);
-    await bc.postMessage({ data: { type: POPUP_RESULT, approve: true } });
+    await bc.postMessage({ data: "topup success" });
+    // await bc.postMessage({ error: "topup failed" });
     bc.close();
   }
 };
