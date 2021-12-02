d4dfffc1d3eaae43faa4885bd4f7b97567c03cc3
cwlee <cwlee0@gmail.com>
2021-12-02 18:02:22 +0800

    feat: use base embed redirect handler for topup

--- src/controllers/TorusController.ts
+++ src/controllers/TorusController.ts
@@ -977,7 +977,14 @@ export default class TorusController extends BaseController<TorusControllerConfi
   async handleTopUp(params: PaymentParams, windowId?: string): Promise<boolean> {
     try {
       const instanceId = windowId || this.getWindowId();
-
+      const instanceState = encodeURIComponent(
+        window.btoa(
+          JSON.stringify({
+            instanceId,
+            provider: "RAMP_NETWORK",
+          })
+        )
+      );
       const parameters = {
         userAddress: params.selectedAddress || this.selectedAddress || undefined,
         userEmailAddress: this.state.PreferencesControllerState.identities[this.selectedAddress].userInfo.email || undefined,
@@ -991,7 +998,7 @@ export default class TorusController extends BaseController<TorusControllerConfi
         hostLogoUrl: "https://app.tor.us/images/torus-logo-blue.svg",
         hostAppName: "Torus",
         hostApiKey: config.rampAPIKEY,
-        finalUrl: `${config.baseRoute}redirect?instanceId=${instanceId}&topup=success`, // redirect url
+        finalUrl: `${config.baseRoute}redirect?state=${instanceState}`, // redirect url
       };
 
       // const redirectUrl = new URL(`${config.baseRoute}/redirect?instanceId=${windowId}&integrity=true&id=${windowId}`);
