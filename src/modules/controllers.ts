fa48850738c3ae94b2989764c55ffa8a3db073ed
cwlee <cwlee0@gmail.com>
2021-12-09 16:51:12 +0800

    fix: call openlogin.logout on logout same behavior as torus wallet add cloneDeep to DefaultState to prevent data mutation

--- src/modules/controllers.ts
+++ src/modules/controllers.ts
@@ -28,6 +28,7 @@ import { cloneDeep, merge, omit } from "lodash-es";
 import log from "loglevel";
 import { Action, getModule, Module, Mutation, VuexModule } from "vuex-module-decorators";
 
+import OpenLoginFactory from "@/auth/OpenLogin";
 import config from "@/config";
 import TorusController, { DEFAULT_CONFIG, DEFAULT_STATE } from "@/controllers/TorusController";
 import installStorePlugin from "@/plugins/persistPlugin";
@@ -46,7 +47,7 @@ import { addToast } from "./app";
   store,
 })
 class ControllerModule extends VuexModule {
-  public torus = new TorusController({ _config: DEFAULT_CONFIG, _state: DEFAULT_STATE });
+  public torus = new TorusController({ _config: DEFAULT_CONFIG, _state: cloneDeep(DEFAULT_STATE) });
 
   public torusState: TorusControllerState = cloneDeep(DEFAULT_STATE);
 
@@ -118,7 +119,7 @@ class ControllerModule extends VuexModule {
 
   @Mutation
   public resetTorusController(): void {
-    this.torus = new TorusController({ _config: DEFAULT_CONFIG, _state: DEFAULT_STATE });
+    this.torus = new TorusController({ _config: DEFAULT_CONFIG, _state: cloneDeep(DEFAULT_STATE) });
   }
 
   @Action
@@ -273,11 +274,25 @@ class ControllerModule extends VuexModule {
   }
 
   @Action
-  logout(): void {
+  async logout(): Promise<Promise<Promise<Promise<void>>>> {
+    if (isMain && this.selectedAddress) {
+      try {
+        const openLoginInstance = await OpenLoginFactory.getInstance();
+        if (openLoginInstance.state.support3PC) {
+          // eslint-disable-next-line no-underscore-dangle
+          openLoginInstance._syncState(await openLoginInstance._getData());
+          await openLoginInstance.logout({ clientId: config.openLoginClientId });
+        }
+      } catch (error) {
+        log.warn(error, "unable to logout with openlogin");
+        window.location.href = "/";
+      }
+    }
     this.updateTorusState(cloneDeep(DEFAULT_STATE));
     const { origin } = this.torus;
-    this.torus.init({ _config: DEFAULT_CONFIG, _state: DEFAULT_STATE });
+    this.torus.init({ _config: DEFAULT_CONFIG, _state: cloneDeep(DEFAULT_STATE) });
     this.torus.setOrigin(origin);
+
     const instanceId = new URLSearchParams(window.location.search).get("instanceId");
     if (instanceId) {
       const logoutChannel = new BroadcastChannel<PopupData<BasePopupChannelData>>(
