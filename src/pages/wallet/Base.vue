a7af2d45233238c6ca91b0154aeb5cd5d85019d8
Ishan Srivastava <ishan@tor.us>
2021-12-28 12:05:47 +0530

    feat : added public address as fallback

--- src/pages/wallet/Base.vue
+++ src/pages/wallet/Base.vue
@@ -20,7 +20,7 @@ onMounted(() => {
   setTimeout(() => {
     showRouterChild.value = true;
   });
-  Sentry.setUser({ email: ControllerModule.torus?.userInfo?.email || "email unknown" });
+  Sentry.setUser({ email: ControllerModule.torus?.userInfo?.email || ControllerModule.torus.selectedAddress });
 });
 </script>
 <template>
