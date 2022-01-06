158649c24f444f18ed226f341828682b3a9db31b
Ishan Srivastava <ishan@tor.us>
2022-01-06 08:51:39 +0530

    Added sentry tracking (#44)
    * feat: rebased and added user email in error
    
    * feat : added public address as fallback
    
    * feat : added last fallback as unknown
    
    * feat : added correct DSN

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
+import { installSentry } from "../sentry";
 import i18nPlugin from "./plugins/i18nPlugin";
 import store from "./store";
 
-createApp(App).use(i18nPlugin).use(router).use(store).mount("#app");
+const vue = createApp(App);
+vue.use(i18nPlugin).use(router).use(store).mount("#app");
+installSentry(vue);
