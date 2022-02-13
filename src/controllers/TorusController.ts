1f1d7623c8faa18a444c3a6acbac11a85c95d06f
cwlee <cwlee0@gmail.com>
2022-02-13 22:08:16 +0800

    fix: default to moonpay disregards embed provider

--- src/controllers/TorusController.ts
+++ src/controllers/TorusController.ts
@@ -1080,7 +1080,9 @@ export default class TorusController extends BaseController<TorusControllerConfi
   async embedhandleTopUp(req: JRPCRequest<TopupInput>): Promise<boolean> {
     const windowId = req.params?.windowId;
     const params = req.params?.params || {};
-    const provider = req.params?.provider || TOPUP.MOONPAY;
+    const provider = TOPUP.MOONPAY;
+    // const provider = req.params?.provider || TOPUP.MOONPAY;
+    // if (!topupPlugin[provider]) throw ethErrors.provider.custom({ code: -32000, message: "Invalid topup provider" });
     return this.handleTopup(provider, params, windowId);
   }
 
