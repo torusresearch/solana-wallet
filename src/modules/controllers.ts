e5cee6a86a7dbff0b2d3245a2cf4233b538a62f0
ArpitAgrawal321 <arpagrawalmail@gmail.com>
2022-02-18 09:15:34 +0530

    feat: added total balance indicator, solana to token display

--- src/modules/controllers.ts
+++ src/modules/controllers.ts
@@ -139,8 +139,21 @@ class ControllerModule extends VuexModule {
     return this.torus.lastTokenRefreshDate;
   }
 
+  get totalBalance(): string {
+    let balance = new BigNumber(0);
+    const selectedCurrency = this.torusState.CurrencyControllerState.currentCurrency;
+    balance = balance.plus(
+      this.fungibleTokens.reduce((sum, curr) => {
+        return sum.plus(new BigNumber((curr.balance?.uiAmount ?? 0) * (curr?.price?.[selectedCurrency.toLowerCase()] ?? 0)));
+      }, new BigNumber(0))
+    );
+    const pricePerToken = this.torusState.CurrencyControllerState.conversionRate;
+    balance = balance.plus(this.solBalance.times(new BigNumber(pricePerToken)));
+    return balance.toFixed(selectedCurrency.toLowerCase() === "sol" ? 4 : 2).toString();
+  }
+
   // user balance in equivalent selected currency
-  get userBalance(): string {
+  get convertedSolBalance(): string {
     const pricePerToken = this.torusState.CurrencyControllerState.conversionRate;
     const selectedCurrency = this.torusState.CurrencyControllerState.currentCurrency;
     const value = this.solBalance.times(new BigNumber(pricePerToken));
