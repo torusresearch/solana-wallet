9ca8c9c1e3e3b2d4954051d4e7b49aa657234c5e
cwlee <cwlee0@gmail.com>
2022-02-21 18:45:33 +0800

    fix: update provider

--- src/plugins/Topup/index.ts
+++ src/plugins/Topup/index.ts
@@ -2,8 +2,8 @@ import { TOPUP, TopUpProvider } from "./interface";
 import moonpay from "./moonpay";
 import ramp from "./ramp";
 
-// export const activeProvider = [TOPUP.MOONPAY];
-export const activeProvider = [TOPUP.MOONPAY, TOPUP.RAMPNETWORK];
+export const activeProvider = [TOPUP.MOONPAY];
+// export const activeProvider = [TOPUP.MOONPAY, TOPUP.RAMPNETWORK];
 
 export const topupPlugin = {
   [TOPUP.RAMPNETWORK]: ramp,
