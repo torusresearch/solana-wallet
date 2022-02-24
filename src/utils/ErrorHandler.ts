90bf478f38b7c6e4c2c31f5788c54297a4c5c81b
Ishan Srivastava <ishan@tor.us>
2022-02-24 22:47:29 +0530

    fix: changed error message, and toast type

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -4,7 +4,7 @@ import { i18n } from "@/plugins/i18nPlugin";
 const { t } = i18n.global;
 export function handleExceptions(e: any): void {
   if (e.exception?.values?.[0]?.value?.includes("Network request failed")) {
-    const rpcNetworkError: any = { type: "error", message: t("walletSettings.changeNetworkError") };
+    const rpcNetworkError: any = { type: "warning", message: t("walletSettings.changeNetworkError") };
     removeToast(rpcNetworkError); // remove existing rpcNetworkError toasts
     addToast(rpcNetworkError); // show rpcNetworkError
   }
