2d0041e9ee927f1d520725ffcd6571cf5f3f5911
Ishan Srivastava <ishan@tor.us>
2022-02-24 22:54:25 +0530

    fix: code structure changes

--- src/plugins/i18nPlugin.ts
+++ src/plugins/i18nPlugin.ts
@@ -41,19 +41,20 @@ export async function loadLocaleMessages(i18n: I18n, locale: string) {
 }
 
 export const setLocale = async (i18n: I18n, lang: string) => {
+  let finalLang = lang;
+
   // if unknown locale is requested, default to en.
   if (!languageMap[lang]) {
     log.error(`REQUESTED UNKNOWN LOCALE ${lang}`);
-    // eslint-disable-next-line no-param-reassign
-    lang = "en";
+    finalLang = "en";
   }
 
   // load locale messages
-  if (!i18n.global.availableLocales.includes(lang)) {
-    await loadLocaleMessages(i18n, lang);
+  if (!i18n.global.availableLocales.includes(finalLang)) {
+    await loadLocaleMessages(i18n, finalLang);
   }
   // set i18n language
-  setI18nLanguage(i18n, lang);
+  setI18nLanguage(i18n, finalLang);
 };
 
 export const i18n = setupI18n();
