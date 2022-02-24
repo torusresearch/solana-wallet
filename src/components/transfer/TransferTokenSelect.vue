58ed15d320d2c19d610b6f39772446187d8235de
ArpitAgrawal321 <arpagrawalmail@gmail.com>
2022-02-24 11:11:59 +0530

    feat: disabled hover colour change on transfer selector

--- src/components/transfer/TransferTokenSelect.vue
+++ src/components/transfer/TransferTokenSelect.vue
@@ -37,7 +37,7 @@ watch(localToken, () => {
     <div class="mt-1 relative">
       <ListboxButton class="bg-white dark:bg-app-gray-800 select-container shadow-inner dark:shadow-none rounded-md w-full px-3">
         <span class="flex items-center">
-          <img :src="selectedToken.iconURL" alt="selected token" class="h-6 w-6" @error="setFallbackImg($event.target, solicon)" />
+          <img :src="selectedToken.iconURL" alt="selected token" class="h-6 w-6 object-cover" @error="setFallbackImg($event.target, solicon)" />
           <span class="ml-3 truncate text-app-text-600 dark:text-app-text-dark-500">
             {{ selectedToken.name || selectedToken?.metaplexData?.offChainMetaData?.name }}
           </span>
@@ -47,73 +47,61 @@ watch(localToken, () => {
         </span>
       </ListboxButton>
 
-      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-        <ListboxOptions
-          class="absolute z-20 mt-1 w-full bg-white dark:bg-app-gray-800 shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-app-gray-400 dark:ring-transparent overflow-auto outline-none focus:outline-none sm:text-sm"
-        >
-          <ListboxOption :key="'tokenstring'" as="template" :value="null" :disabled="true">
-            <li class="option-separator">
-              <img class="h-4 w-auto" :src="SolTokenLogo" alt="Tokens" />
-              <p class="ml-2 text-sm text-app-text-400 dark:text-app-text-dark-400">
-                {{ t("walletTransfer.tokens") }}
-              </p>
-            </li>
-          </ListboxOption>
-          <ListboxOption v-for="item in tokens" v-slot="{ active, selected }" :key="item.name" as="template" :value="item">
-            <li
-              :class="[
-                active ? 'bg-app-gray-200' : '',
-                'cursor-pointer select-none relative py-2 px-9 text-app-text-600 dark:text-app-text-dark-500 dark:hover:text-app-text-600',
-              ]"
-            >
+      <ListboxOptions
+        class="absolute z-20 mt-1 w-full bg-white dark:bg-app-gray-800 shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-app-gray-400 dark:ring-transparent overflow-auto outline-none focus:outline-none sm:text-sm"
+      >
+        <ListboxOption :key="'tokenstring'" as="template" :value="undefined" :disabled="true">
+          <li class="option-separator">
+            <img class="h-4 w-auto object-cover" :src="SolTokenLogo" alt="Tokens" />
+            <p class="ml-2 text-sm text-app-text-400 dark:text-app-text-dark-400">
+              {{ t("walletTransfer.tokens") }}
+            </p>
+          </li>
+        </ListboxOption>
+        <ListboxOption v-for="item in tokens" v-slot="{ selected }" :key="item.name" as="template" :value="item">
+          <li :class="['cursor-pointer select-none relative py-2 px-9 text-app-text-600 dark:text-app-text-dark-500']">
+            <div class="flex items-center">
+              <img :src="item?.iconURL" class="h-6 w-6 object-cover" alt="iconURI" @error="setFallbackImg($event.target, solicon)" />
+              <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 truncate']" class="token-name">
+                <p>{{ item?.name }} ({{ item?.symbol }})</p>
+                <p class="text-app-gray-500">
+                  {{ item?.symbol === "SOL" ? "" : "SPL" }}
+                </p></span
+              >
+            </div>
+          </li>
+        </ListboxOption>
+        <li class="option-separator">
+          <img class="h-4 object-cover" :src="NftLogo" alt="Tokens" />
+          <p class="ml-2 text-sm text-app-text-400 dark:text-app-text-dark-400">NFTS</p>
+        </li>
+        <template v-for="item in getClubbedNfts(nftTokens)" :key="item.title">
+          <li class="py-2 px-9">
+            <p class="text-sm text-app-text-400 dark:text-app-text-dark-400">{{ item.title }} ({{ item.count }})</p>
+          </li>
+          <ListboxOption
+            v-for="mintAddress in item.mints"
+            v-slot="{ selected }"
+            :key="mintAddress"
+            as="template"
+            :value="getTokenFromMint(nftTokens, mintAddress)"
+          >
+            <li :class="['cursor-pointer select-none relative py-2 pl-9 pr-9 text-app-text-600 dark:text-app-text-dark-500 ml-4']">
               <div class="flex items-center">
-                <img :src="item?.iconURL" class="h-6 w-6 rounded-full" alt="iconURI" @error="setFallbackImg($event.target, solicon)" />
-                <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 truncate']" class="token-name">
-                  <p>{{ item?.name }} ({{ item?.symbol }})</p>
-                  <p class="text-app-gray-500">
-                    {{ item?.symbol === "SOL" ? "" : "SPL" }}
-                  </p></span
-                >
+                <img
+                  :src="getTokenFromMint(nftTokens, mintAddress)?.iconURL"
+                  class="h-6 w-6 object-cover"
+                  alt="iconURI"
+                  @error="setFallbackImg($event.target, FallbackNft)"
+                />
+                <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate']" class="token-name">
+                  <p>{{ getTokenFromMint(nftTokens, mintAddress)?.name }} ({{ getTokenFromMint(nftTokens, mintAddress)?.symbol }})</p>
+                </span>
               </div>
             </li>
           </ListboxOption>
-          <li class="option-separator">
-            <img class="h-4" :src="NftLogo" alt="Tokens" />
-            <p class="ml-2 text-sm text-app-text-400 dark:text-app-text-dark-400">NFTS</p>
-          </li>
-          <template v-for="item in getClubbedNfts(nftTokens)" :key="item.title">
-            <li class="py-2 px-9">
-              <p class="text-sm text-app-text-400 dark:text-app-text-dark-400">{{ item.title }} ({{ item.count }})</p>
-            </li>
-            <ListboxOption
-              v-for="mintAddress in item.mints"
-              v-slot="{ active, selected }"
-              :key="mintAddress"
-              as="template"
-              :value="getTokenFromMint(nftTokens, mintAddress)"
-            >
-              <li
-                :class="[
-                  active ? 'bg-app-gray-200' : '',
-                  'cursor-pointer select-none relative py-2 pl-9 pr-9 text-app-text-600 dark:text-app-text-dark-500 dark:hover:text-app-text-600 ml-4',
-                ]"
-              >
-                <div class="flex items-center">
-                  <img
-                    :src="getTokenFromMint(nftTokens, mintAddress)?.iconURL"
-                    class="h-6 w-6 rounded-full"
-                    alt="iconURI"
-                    @error="setFallbackImg($event.target, FallbackNft)"
-                  />
-                  <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate']" class="token-name">
-                    <p>{{ getTokenFromMint(nftTokens, mintAddress)?.name }} ({{ getTokenFromMint(nftTokens, mintAddress)?.symbol }})</p>
-                  </span>
-                </div>
-              </li>
-            </ListboxOption>
-          </template>
-        </ListboxOptions>
-      </transition>
+        </template>
+      </ListboxOptions>
     </div>
   </Listbox>
 </template>
