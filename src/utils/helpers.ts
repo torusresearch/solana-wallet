de00b61e4d466f70d620eba94e49106ea2d2c102
cwlee <cwlee0@gmail.com>
2022-03-07 12:26:21 +0800

    fix: helper files

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -230,3 +230,19 @@ export const MintLayout = borsh.struct([
   borsh.u32("freezeAuthorityOption"),
   borsh.publicKey("freezeAuthority"),
 ]);
+
+export const parseJwt = (token: string) => {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    Buffer.from(base64, "base64")
+      .toString()
+      .split("")
+      .map((c) => {
+        return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+};
