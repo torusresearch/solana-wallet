b3cf5c0bca2d35561f0b4b7fe543cec0990f0435
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2022-03-09 14:27:55 +0530

    fix: broken top up tests

--- tests/e2e/topup/topup.test.ts
+++ tests/e2e/topup/topup.test.ts
@@ -2,29 +2,33 @@
 import test, { expect } from "@playwright/test";
 
 import { login } from "../../auth-helper";
-import { changeLanguage, ensureTextualElementExists, getInnerText, switchTab } from "../../utils";
+import { changeLanguage, ensureTextualElementExists, getInnerText, switchTab, wait } from "../../utils";
 
 test("Popup Page Should render", async ({ context }) => {
   const page = await login(context);
 
   // // see navigation works correctly
   await switchTab(page, "topup");
-
-  // RAMP TOPUP SHOULD WORK AS EXPECTED
-  // set amount to be transfered as 100 US Dollars, expect a positive value for expected SOL
-  await page.click("img[alt=Ramp]");
+  // ENSURE UI IS INTACT
+  await ensureTextualElementExists(page, "Select a Provider");
+  // MoonPay SHOULD WORK AS EXPECTED
+  // set amount to be transferred as 100 US Dollars, expect a positive value for expected SOL
+  await page.click("img[alt=moonpay]");
   await page.fill("input[type='number']", "100");
   const usdToSol100 = Number(await getInnerText(page, "#resCryptoAmt"));
   expect(usdToSol100).toBeGreaterThan(0);
 
-  // set amount to be transfered as 200 US Dollars, expect a positive value for expected SOL
+  // set amount to be transferred as 200 US Dollars, expect a positive value for expected SOL
   await page.fill("input[type='number']", "200");
   const usdToSol200 = Number(await getInnerText(page, "#resCryptoAmt"));
   expect(usdToSol200).toBeGreaterThan(0);
 
-  // ensure that on clicking save, it is refirected to Ramp Payment page having torus logo
-  await Promise.all([page.click("button:has-text('Top up')"), page.waitForEvent("popup")]);
-  await expect((await page.$$("img[alt=Ramp]")).length).toEqual(1);
+  // ensure that on clicking Top up, it is redirected to MoonPay Payment page
+  const [page2] = await Promise.all([page.waitForEvent("popup"), page.click("button:has-text('Top up')")]);
+  // closing moonpay page, will show an error on top up page
+  page2.close();
+  await wait(500);
+  await ensureTextualElementExists(page, "Transaction could not complete.");
 });
 
 test("Language change should work", async ({ context }) => {
@@ -32,18 +36,23 @@ test("Language change should work", async ({ context }) => {
   await switchTab(page, "topup");
 
   await changeLanguage(page, "german");
+  await wait(500);
   await ensureTextualElementExists(page, "Wählen Sie einen Anbieter");
 
   await changeLanguage(page, "japanese");
+  await wait(500);
   await ensureTextualElementExists(page, "プロバイダーを選択");
 
   await changeLanguage(page, "korean");
+  await wait(500);
   await ensureTextualElementExists(page, "공급자를 선택하십시오");
 
   await changeLanguage(page, "mandarin");
+  await wait(500);
   await ensureTextualElementExists(page, "选择供应商");
 
   await changeLanguage(page, "spanish");
+  await wait(500);
   await ensureTextualElementExists(page, "Selecciona un Proveedor");
 
   await changeLanguage(page, "english");
