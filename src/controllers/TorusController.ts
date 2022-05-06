14015a27b6e4279fe5260e272026985cdd7ddf3e
Tai Nguyen <tai.nguyen@tor.us>
2022-05-06 17:34:19 +0700

    feat: refactor code

--- src/controllers/TorusController.ts
+++ src/controllers/TorusController.ts
@@ -1220,7 +1220,7 @@ export default class TorusController extends BaseController<TorusControllerConfi
       const keypair = getED25519Key(paddedKey);
       const address = await this.addAccount(base58.encode(keypair.sk), userInfo);
 
-      await this.getAppScopedKeys(privKey, userInfo);
+      await this.importAppScopedKeys(privKey, userInfo);
 
       this.setSelectedAccount(address);
       if (!this.hasSelectedPrivateKey) throw new Error("Wallet Error: Invalid private key ");
@@ -1394,7 +1394,7 @@ export default class TorusController extends BaseController<TorusControllerConfi
         }
 
         // derived app-scoped keys
-        if (decryptedState.openloginPrivateKey) await this.getAppScopedKeys(decryptedState.openloginPrivateKey, decryptedState.userInfo!);
+        if (decryptedState.openloginPrivateKey) await this.importAppScopedKeys(decryptedState.openloginPrivateKey, decryptedState.userInfo!);
 
         // This call sync and refresh blockchain state
         this.setSelectedAccount(address, true);
@@ -1670,7 +1670,7 @@ export default class TorusController extends BaseController<TorusControllerConfi
     this.embedController.initializeProvider(commProviderHandlers);
   }
 
-  private async getAppScopedKeys(privKey: string, userInfo: UserInfo) {
+  private async importAppScopedKeys(privKey: string, userInfo: UserInfo) {
     try {
       const torus = new Torus();
       const ethAddress = torus.generateAddressFromPrivKey(new BN(privKey, "hex"));
