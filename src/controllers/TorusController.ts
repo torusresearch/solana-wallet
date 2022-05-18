ffcc9588fdf4e5aaf753c4076d5367d7617c54ba
cwlee <cwlee0@gmail.com>
2022-05-18 17:22:46 +0800

    fix: prevent custom rpc node on Main wallet

--- src/controllers/TorusController.ts
+++ src/controllers/TorusController.ts
@@ -330,9 +330,11 @@ export default class TorusController extends BaseController<TorusControllerConfi
 
     const initialState = _state;
     // overwrite google node with default node
+    // overwrite custom node with default node when isMain is True
     if (
-      initialState.NetworkControllerState?.providerConfig?.rpcTarget?.includes("api.google") &&
-      (isMain || !initialState.NetworkControllerState.isCustomNetwork)
+      (initialState.NetworkControllerState?.providerConfig?.rpcTarget?.includes("api.google") &&
+        !initialState.NetworkControllerState.isCustomNetwork) ||
+      (isMain && initialState.NetworkControllerState?.isCustomNetwork)
     ) {
       let defaultNetwork = WALLET_SUPPORTED_NETWORKS.mainnet;
       if (initialState.NetworkControllerState.providerConfig.chainId === "0x02") defaultNetwork = WALLET_SUPPORTED_NETWORKS.testnet;
