d9d56547edbc1a46be6e9ed838babe6ccaa04aae
cwlee <cwlee0@gmail.com>
2022-07-07 16:04:03 +0800

    fix: set amount=0 when not selectedAddress

--- src/utils/solanaHelpers.ts
+++ src/utils/solanaHelpers.ts
@@ -205,7 +205,9 @@ export async function calculateChanges(
   // compare post token account with current token account.
   postTokenDetails.forEach(async (item, idx) => {
     // Track only tokenDetail related to selectedAddress (pre or post)
-    if (preTokenDetails[idx]?.owner.toBase58() === selectedAddress || item.owner.toBase58() === selectedAddress) {
+    const preTokenOwner = preTokenDetails[idx]?.owner.toBase58();
+    const postTokenOwner = item.owner.toBase58();
+    if (preTokenOwner === selectedAddress || postTokenOwner === selectedAddress) {
       let mint = item.mint.toBase58();
       const symbol = addressSlicer(item.mint.toBase58());
 
@@ -225,8 +227,13 @@ export async function calculateChanges(
       // else throw error ?
 
       // const { decimals } = mintAccountInfos[idx];
-      const preTokenAmount = preTokenDetails[idx]?.amount || BigInt(0);
-      const changes = Number(item.amount - preTokenAmount) / 10 ** decimals;
+      let postAmount = item.amount;
+      if (postTokenOwner !== selectedAddress) postAmount = BigInt(0);
+
+      let preTokenAmount = preTokenDetails[idx]?.amount || BigInt(0);
+      if (preTokenOwner !== selectedAddress) preTokenAmount = BigInt(0);
+
+      const changes = Number(postAmount - preTokenAmount) / 10 ** decimals;
 
       returnResult.push({
         changes: Number(changes.toString()),
