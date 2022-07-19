8506854336444b80ee87a2ba4b33bf90d9ebc107
Guru Ramu <guru@tor.us>
2022-07-19 15:50:10 +0530

    fix: white label theme

--- src/utils/whitelabel.ts
+++ src/utils/whitelabel.ts
@@ -1,7 +1,5 @@
 import color from "color";
 
-import SolanaLogoURL from "@/assets/solana-dark.svg";
-import SolanaLightLogoURL from "@/assets/solana-light.svg";
 import { WhiteLabelParams } from "@/utils/interfaces";
 import { NAVIGATION_LIST } from "@/utils/navHelpers";
 
@@ -66,9 +64,6 @@ export function getWhiteLabelLogoDark() {
 
 export function getWhiteLabelLogo(isDark = true): string {
   return (isDark ? getWhiteLabel()?.logoDark : getWhiteLabel()?.logoLight) || "";
-}
-export function getLogo(isDark = true) {
-  return isDark ? getWhiteLabelLogoLight() || SolanaLightLogoURL : getWhiteLabelLogoDark() || SolanaLogoURL;
 }
 
 export function getWhiteLabelLocale(): string {
