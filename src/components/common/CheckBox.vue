e6dc7f79cca560f440c5b757efd650d8c29108f0
Guru Ramu <guru@tor.us>
2022-08-23 09:29:08 +0530

    fix: css

--- src/components/common/CheckBox.vue
+++ src/components/common/CheckBox.vue
@@ -15,12 +15,12 @@ withDefaults(
 <template>
   <div>
     <div class="form-check">
-      <label for="checkbox" class="flex items-center form-check-label text-app-text-600 dark:text-app-text-dark-500">
+      <label for="checkbox" class="flex items-center text-sm form-check-label text-app-text-600 dark:text-app-text-dark-500">
         <input
           id="checkbox"
           :v-model="checked"
           type="checkbox"
-          class="form-check-input mr-2 appearance-none h-4 w-4 border border-gray-300 focus:outline-app-primary-500 rounded-sm bg-white checked:bg-app-primary-500 checked:border-app-primary-500 checked:outline-app-primary-500 checked:text-app-primary-500 cursor-pointer"
+          class="form-check-input mr-1 appearance-none h-4 w-4 border border-gray-300 focus:outline-app-primary-500 rounded-sm bg-white checked:bg-app-primary-500 checked:border-app-primary-500 checked:outline-app-primary-500 checked:text-app-primary-500 cursor-pointer"
           value=""
         />
         {{ label }}
